9348122c76f3ca101e065bc3c60d2e35f1744342
Jamal BOUIZEM <jbouizem@gmail.com>
2014-06-27 00:10:38 +0200

    RC RoomyHouse Version

---
+++ Sources/Assets/Scripts/InitLevelLoaded.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NetworkView))]
+public class InitLevelLoaded : MonoBehaviour 
+{
+	private string disconnectedLevel = "Lobby";
+	
+	void FixedUpdate() 
+	{
+		if(Network.peerType == NetworkPeerType.Disconnected)
+		{
+			Network.RemoveRPCs(Network.player);
+			Network.DestroyPlayerObjects(Network.player);
+			Application.LoadLevel(disconnectedLevel);
+		}
+	}
+}
